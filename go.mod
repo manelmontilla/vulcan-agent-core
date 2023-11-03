8c03eee4559087cea401f1361617f0c87390c01e
Manel Montilla <manel.montilla@gmail.com>
2023-11-03 19:07:36 +0100

    all: bootstrap repository

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adevinta/vulcan-agent-core
+
+go 1.21.3
---
+++ placeholder.go
@@ -0,0 +1,4 @@
+// Copyright 2023 Adevinta
+
+// Package vulcanagentcore is just an initial placeholder.
+package vulcanagentcore
